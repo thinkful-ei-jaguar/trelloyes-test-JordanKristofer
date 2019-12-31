4b7b93a2207efe86de04dfc719dbabad33de43b4
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-31 12:10:00 -0800

    set up snapshot tests

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import Card from './Card.js';
 
-//Smoke Testing
-it('renders without crashing', () => {
-  //DOM element to render the component into
-  const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Card />', () => {
+  //Smoke Testing
+  it('renders without crashing', () => {
+    //DOM element to render the component into
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //Snapshot Testing
+  it('renders this UI as expected', () => {
+    //render the component as JSON
+    const tree = renderer
+      .create(<Card />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
+
--- src/List.test.js
+++ src/List.test.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import List from './List.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <List 
-      // header='TEST HEADER'
-      cards={[
-        {id: 1, title:'TEST 1 Title', content: 'Test is working!'},
-        {id: 2, title: 'TEST 2 Title', content: 'Test 2 is wokring!'}
-      ]}
-    />, 
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('<List />', () => {
+  //Smoke Testing 
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <List 
+        // header='TEST HEADER'
+        cards={[
+          {id: 1, title:'TEST 1 Title', content: 'Test is working!'},
+          {id: 2, title: 'TEST 2 Title', content: 'Test 2 is wokring!'}
+        ]}
+      />, 
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //Snapshot Testing 
+  it('renders this UI as expected', () => {
+    //render the component as JSON
+    const tree = renderer
+      .create(<List />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
